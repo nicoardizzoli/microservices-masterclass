f0009d7f8005e32d20cdbc24cf0cbcde249baefb
nicoardizzoli <nicolas.ardizzoli@gmail.com>
2022-04-19 23:42:28 -0300

    added kubernetes configuration to deploy microservices in pods

--- customer/src/main/java/com/nicoardizzoli/customer/CustomerApplication.java
+++ customer/src/main/java/com/nicoardizzoli/customer/CustomerApplication.java
@@ -2,8 +2,9 @@ package com.nicoardizzoli.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication(
         //esto es para poder inyectar las clases q esten en esos packages
@@ -12,9 +13,14 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
                 "com.nicoardizzoli.customer"
         }
 )
-@EnableEurekaClient
+//@EnableEurekaClient
 @EnableFeignClients(
         basePackages = "com.nicoardizzoli.clients"
+)
+
+//esto es para leer las properties de clients
+@PropertySources(
+        @PropertySource("classpath:clients-${spring.profiles.active}.properties")
 )
 public class CustomerApplication {
     public static void main(String[] args) {
--- notification/src/main/java/com/nicoardizzoli/notification/NotificationApplication.java
+++ notification/src/main/java/com/nicoardizzoli/notification/NotificationApplication.java
@@ -2,13 +2,17 @@ package com.nicoardizzoli.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication(scanBasePackages = {
         "com.nicoardizzoli.notification",
         "com.nicoardizzoli.amqp"
 })
-@EnableEurekaClient
+//@EnableEurekaClient
+@PropertySources(
+        @PropertySource("classpath:clients-${spring.profiles.active}.properties")
+)
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
